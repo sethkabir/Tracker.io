5930497315d2aebb7427399dc5f6fb4ed5228f0d
Kabir Seth <seth.kabir14@gmail.com>
2021-12-30 23:32:17 +0530

    optimize server side routing using Navigate

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,12 +3,13 @@ import Home from "./Components/Home";
 import MapPage from "./Components/MapPage";
 import Profile from "./Components/Profile";
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element = {<Navigate replace to = "/login" />} /> 
         <Route path="/" element = {<Register />}>
           <Route path="/login" element = {<Login />} />
           <Route path="/signup" element = {<SignUp />} />
