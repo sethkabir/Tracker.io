18177f465217a7fe3099760def76e233f2b4183f
Kabir Seth <seth.kabir14@gmail.com>
2022-01-04 15:33:54 +0530

    advanced urls pattern

--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -13,15 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from .views import index
+
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('tracker.urls')),
-    path('', include('frontend.urls')),
-    # path('auth', index),
-    # path('dashboard', index)
-
+    path('auth', index),
+    path('dashboard', index)
 ]
