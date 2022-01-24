d3ca60a5ad2e5bfbec6efce43a320b9f58be9255
shivang <shivang.mathur1@gmail.com>
2022-01-24 21:13:52 +0530

    Add password validation

--- src/tracker/serializers.py
+++ src/tracker/serializers.py
@@ -1,9 +1,11 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password as pasval
 
 from rest_framework import serializers
 
 User = get_user_model()
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -13,6 +15,14 @@ class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password', 'first_name', 'last_name', 'email']
+    
+    def validate_password(self, value):
+        try:
+            pasval(value)
+        except ValidationError as exc:
+            raise serializers.ValidationError(str(exc))
+        return value
+
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
@@ -25,4 +35,4 @@ class SignupSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
-        
+
