b6191f5eeaaff7b3d213f33f8117b7c70caf1b3e
Kabir <seth.kabir14@gmail.com>
2022-01-24 23:40:51 +0530

    add landing page

--- src/frontend/src/Components/LandingPage.js
+++ src/frontend/src/Components/LandingPage.js
@@ -1,15 +1,14 @@
 import { Link} from "react-router-dom";
-import bird from '../Images/bird.png'
+import bike from '../Images/bike.gif'
 
 const LandingPage = () => {
   return (
-    <div>
-      "hello everynyan, how are you? fine thank you!, oh mai gah!, i wish i were
-      a bird"
-      <img src={bird} alt="this is a bird"/>
+    <div className="flex flex-col bg-primary h-screen place-items-center">
+      <div className="text-5xl text-white mt-20">Bike Buddy</div>
+      <img src={bike} alt="this is a bird "/>
       <Link to="/auth/login">
-        <button className="bg-blue-300 h-10 w-20 rounded-lg mx-3">
-          log in
+        <button className="bg-blue-300 h-14 w-36 rounded-lg mx-3 text-lg mt-10 sm:mt-0">
+          Get Started!
         </button>
       </Link>
     </div>
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#1a1819',
+      }
+    },
   },
   plugins: [],
 };
