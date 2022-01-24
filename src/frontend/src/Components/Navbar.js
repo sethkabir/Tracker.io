970c49c98eb55f8c612193439d40a07edf387be4
Kabir <seth.kabir14@gmail.com>
2022-01-25 00:26:44 +0530

    add change password, modify logout button

--- src/frontend/src/Components/Navbar.js
+++ src/frontend/src/Components/Navbar.js
@@ -85,16 +85,13 @@ const Navbar = (props) => {
             </Link>
           </div>
           <div className="py-1">
-            <button onClick={logout} className="group flex itme px-4 py-2 text-sm text-black hover:bg-blue-400 hover:text-white">
+            <button
+              onClick={logout}
+              className="group flex itme px-4 py-2 text-sm text-black hover:bg-blue-400 hover:text-white w-full"
+            >
               <LogoutIcon className="mr-3 h-5 w-5 text-gray-400 group-hover:text-white" />
               Logout
             </button>
-          </div>
-          <div className="py-1">
-            <div className="group flex itme px-4 py-2 text-sm text-black hover:bg-blue-400 hover:text-white">
-              <LogoutIcon onClick={logout} className="mr-3 h-5 w-5 text-gray-400 group-hover:text-white" />
-              Logout
-            </div>
           </div>
         </div>
       )}
