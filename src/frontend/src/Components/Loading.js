6fdc2a516ea4729a4fda388e9d0354689aa50331
Kabir <seth.kabir14@gmail.com>
2022-01-26 15:15:51 +0530

    add discord Oauth2 frontend

--- src/frontend/src/Components/Loading.js
+++ src/frontend/src/Components/Loading.js
@@ -1,5 +1,7 @@
 //loading screen post discord authorization
 import axios from "axios";
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Loading = () => {
   //extract the current url
@@ -13,59 +15,47 @@ const Loading = () => {
   params.append("grant_type", "authorization_code");
   params.append("code", code);
   params.append("redirect_uri", "http://127.0.0.1:3000/auth/discord");
-
-  //custom header (provided by discord documenation)
-  const config = {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Accept: "application/json",
-    },
-  };
+  params.append("scope", "identify");
 
   //POST REQUEST (to obtain the access token)
-  let token;
-  axios(
-    {
-      method: "post",
-      url: "https://discord.com/api/v8/oauth2/token",
-      data: params,
-    },
-    config
-  )
-    .then((res) => {
-      console.log(res.data.access_token);
-      token = res.data.access_token;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  const [access_token, setAccsessToken] = useState(null);
+  // const [response, setResponse] = useState(null);
+  // GET REQUEST (to send the obtained token back to discord for authentication)
 
-  const getconfig = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+  useEffect(() => {
+    axios
+      .post("https://discord.com/api/v8/oauth2/token", params, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then((res) => {
+        setAccsessToken(res.data.access_token);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [useLocation().pathname]);
 
-  //GET REQUEST (to send the obtained token back to discord for authentication)
-  axios(
-    {
-      method: "get",
-      url: "https://discord.com/api/v8/users/@me",
-    },
-    getconfig
-  )
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get("https://discord.com/api/v8/users/@me", {
+        headers: {
+          authorization: `Bearer ${access_token}`,
+        },
+      })
+      .then((data) => {
+        navigate("/dashboard/home");  
+      });
+  }, [access_token]);
 
-  //main screen (maybe add landing page??????)
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-primary opacity-75 flex flex-col items-center justify-center">
       <h2 className="flex animate-pulse">
-        <div className="text-center text-white text-xl font-semibold ">Loading</div>
+        <div className="text-center text-white text-xl font-semibold ">
+          Loading
+        </div>
         <div className="loader ease-linear rounded-full border-3 border-t-4 border-gray-200 h-1 w-1 ml-1 mb-2  place-self-end"></div>
         <div className="loader ease-linear rounded-full border-3 border-t-4 border-gray-200 h-1 w-1 ml-1 mb-2  place-self-end"></div>
         <div className="loader ease-linear rounded-full border-3 border-t-4 border-gray-200 h-1 w-1 ml-1 mb-2  place-self-end"></div>
