3b85d91064286083476962105c3c4402490edf47
shivang <shivang.mathur1@gmail.com>
2022-01-27 14:52:08 +0530

    Add daisy ui

--- src/frontend/src/Components/EmergencyContact.js
+++ src/frontend/src/Components/EmergencyContact.js
@@ -1,5 +1,22 @@
+import Navbar from './Navbar';
+
 const EmergencyContact = () => {
-    return <div>hello</div>
+    return (
+    <div className="flex flex-col h-screen">
+        <Navbar />
+        <div className="card card-bordered">
+        <div class="card-body">
+            <h2 class="card-title">Top image
+                <div class="badge mx-2 badge-secondary">NEW</div>
+            </h2> 
+            <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
+            <div class="justify-end card-actions">
+                <button class="btn btn-secondary">More info</button>
+            </div>
+        </div>
+        </div>
+    </div>
+    )
 }
 
 
