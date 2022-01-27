56927790c2b9be143dd3688040144bf9dfe55a51
Kabir <seth.kabir14@gmail.com>
2022-01-27 14:27:32 +0530

    add current geolocation to map

--- src/frontend/src/Components/MapComponent.js
+++ src/frontend/src/Components/MapComponent.js
@@ -1,6 +1,6 @@
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, {GeolocateControl} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useState, useEffect } from "react";
+import { useState} from "react";
 
 //this is to resolve the web error which happens due to mapbox integration when using npm run build!
 // added the following 6 lines.
@@ -9,33 +9,22 @@ import mapboxgl from "mapbox-gl";
 // notice the exclamation point in the import.
 // @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+mapboxgl.workerClass =require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
+
+const geolocateControlStyle = {
+  right: 10,
+  top: 10
+};
 const MapComponent = () => {
   //the following code obtains the current location for the user at regular intervals of time and updates them
-
-  const [userlocation, setUserLocation] = useState(null);
-  const [locationerror, setLocationError] = useState(null);
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      setLocationError(null);
-
-      navigator.geolocation.watchPosition((position) => {
-        setUserLocation(position);
-      });
-    } else {
-      setUserLocation(null);
-      setLocationError("unable to access users current location");
-    }
-  }, [userlocation, locationerror]);
 
   //mapbox documentation!
   const [viewport, setViewport] = useState({
     width: "100%",
     height: "100%",
-    latitude: userlocation ? userlocation.coords.latitude : 0,
-    longitude: userlocation ? userlocation.coords.longitude : 0,
+    latitude: 0,
+    longitude: 0,
     zoom: 8,
   });
 
@@ -47,7 +36,14 @@ const MapComponent = () => {
         mapboxApiAccessToken="pk.eyJ1IjoiZW5pZ21vNDIiLCJhIjoiY2t5NnRxeDdwMHo0czJ4b244dzcxN21rdCJ9.L6vD5im_03cWtFc6b7HZYg"
         {...viewport}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      ></ReactMapGL>
+      >
+        <GeolocateControl
+        style={geolocateControlStyle}
+        positionOptions={{enableHighAccuracy: true}}
+        trackUserLocation={true}
+        auto
+      />
+      </ReactMapGL>
     </div>
   );
 };
